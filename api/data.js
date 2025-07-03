cf5f0e45698b315d055c8e01bb4bfc4365207e43
naBogado <bogado.nico17@gmail.com>
2025-07-03 15:23:53 -0300

    Correccion de errores

--- api/data.js
+++ api/data.js
@@ -1,6 +1,7 @@
 export default async function handler(req, res) {
   const apiKey = process.env.CAT_API_KEY;
-  const baseUrl = "https://api.thecatapi.com/v1/images/search?has_breeds=1";
+  const baseUrl =
+    "https://api.thecatapi.com/v1/images/search?limit=10&has_breeds=1";
 
   const params = new URLSearchParams(req.query);
   params.append("api_key", apiKey);
